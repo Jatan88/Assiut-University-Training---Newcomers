c6008b41025042d0807025b87f9424950de36e2a
Jatan88 <61798906+Jatan88@users.noreply.github.com>
2022-02-28 01:25:43 +0530

    Create Sheet 2 - F. Multiplication Table.cpp

---
+++ Sheet 2 - F. Multiplication Table.cpp
@@ -0,0 +1,23 @@
+/*
+
+Solution By Jatan Shah
+CF Handle : Jatan88
+Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/F
+Status : Accepted
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 0;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1; i<=12; i++) cout<<n<<" * "<<i<<" = "<<n*i<<"\n";
+        
+    
+    return 0;
+}
