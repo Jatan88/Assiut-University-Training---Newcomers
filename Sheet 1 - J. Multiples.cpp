225c5aa9e0f79ce47eb71397d1a9e254f6a4b3df
Jatan88 <61798906+Jatan88@users.noreply.github.com>
2022-02-28 01:23:16 +0530

    Create Sheet 1 - J. Multiples.cpp

---
+++ Sheet 1 - J. Multiples.cpp
@@ -0,0 +1,24 @@
+/*
+
+Solution By Jatan Shah
+CF Handle : Jatan88
+Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/J
+Status : Accepted
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 0;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a % b == 0 || b % a == 0) cout<<"Multiples"<<"\n";
+    else cout<<"No Multiples"<<"\n";
+        
+    
+    return 0;
+}
